88591a43566dadc445ece410475bdecc774e0d71
enriquealegre <enriquealegreal@gmail.com>
2024-04-24 10:48:32 -0300

    agregue funcion

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+// La funcion es saludar al usuario antes de jugar
+
+function saludarMaradona() {    
+
+    let nombreDeMaradona = prompt("Hola crack ingresa tu nombre para seguir adelante!")
+        alert("Lo haras genial " + nombreDeMaradona);
+        return;
+      }
+    
+saludarMaradona();
